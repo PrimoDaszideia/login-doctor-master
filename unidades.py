22f755f1dc3bd78976e84c775097e67e28e0d691
Lucas Silva <lucasprimoprofessional@gmail.com>
2025-05-14 14:29:07 -0300

    fazendo mudanças

--- unidades.py
+++ unidades.py
@@ -1,10 +1,24 @@
 unidadesEmails = (
 
     {
-    'unidade': "skks",
-    'email': "sjjsjs",
-    'cnpj': "sjsjs",
-    'servidor': "sksksks"
+    'unidade': "teste",
+    'email': "teste.com",
+    'cnpj': "00.000.000/0000-00",
+    'servidor': "jp"
+    },
+    
+    {
+     'unidade': "jjj",
+     'email': "jjj",
+     'cnpj': "jjj",
+     'servidor': "jjj"
+    },
+
+    {
+     'unidade': "Odonto Modelo",
+     'email': "odontomodelo.com",
+     'cnpj': "37.207.702/0001-5",
+     'servidor': "PB"
     },
     
 )
