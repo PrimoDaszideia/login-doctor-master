8c17eb3a166aadc50db39e3e28a48313b93de944
Luiz-Neto18 <luizneto23ago@gmail.com>
2025-05-16 22:26:32 -0300

    Novas Unidades

--- unidades.py
+++ unidades.py
@@ -20,5 +20,38 @@ unidadesEmails = (
      'cnpj': "37.207.702/0001-5",
      'servidor': "PB"
     },
-    
-)
+
+    {
+     'unidade': "Clinica Sugisawa",
+     'email': "clinicasugiwa.com",
+     'cnpj': "80.352.925/0001-30",
+     'servidor': "PR"
+    },
+
+    {
+     'unidade': "Onmed Hospitalar LTDA",
+     'email': "onmedhospitalar.com",
+     'cnpj': "47.314.068/0001-09",
+     'servidor': "RJ"
+    },
+
+    {
+     'unidade': "Hospital e Maternidade Sotero de Souza",
+     'email': "hmss.com",
+     'cnpj': "66.518.267/0038-75",
+     'servidor': "SP"
+    },
+
+    {
+     'unidade': "Hifa - Pronto Atendimento Octacilio Geraldo do Carmo",
+     'email': "paogc.com",
+     'cnpj': "27.192.590/0007-43",
+     'servidor': "ES"
+    },
+
+    {
+     'unidade': "Dr Saude",
+     'email': "drsaude.com",
+     'cnpj': "53.252.299/0001-38",
+     'servidor': "DF"
+    },
