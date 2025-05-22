79ec9c608ff15d4a84bd11c5c106bd4eeeec4d45
Lucas Silva <lucasprimoprofessional@gmail.com>
2025-05-22 10:51:17 -0300

    Aprendi a colocar imagens nos Button

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import Tk
+import tkinter as tk
+from PIL import Image, ImageTk
 
 
 # Janela pricipal
@@ -9,16 +11,22 @@ appMain.title("Login Doctor Master")
 appMain.iconbitmap("img/surgeon.ico")
 appMain.geometry("800x450")
 
+#Imagens do sistema
+
+icone = Image.open("img/surgeon.png")       # Substitua pelo caminho da sua imagem
+icone = icone.resize((24, 24))            # Redimensiona o ícone, se necessário
+icone_tk = ImageTk.PhotoImage(icone)
+
 # Seja Bem-Vindo
 titulo = ttk.Label(appMain, text="Seja Bem-Vindo!", font=("Arial", 14))
 titulo.pack(pady=10)
 
+
 #entrada de nomes
-title_name = ttk.Label(appMain, text="Nome do Especialista")
+title_name = ttk.Label(appMain, text="Nome do Especialista:")
 title_name.pack(pady= 4)
 entry_name = ttk.Entry(appMain, width= 35)
 entry_name.pack(pady= 1)
-
 
 #opções de login
 '''title_opcoes = ["Dr." ,"Dra.","Recepcionista"]
@@ -30,10 +38,19 @@ ttk.Label(appMain, text= "Especialidade: ").pack()
 entry_especialidade = Entry(appMain, width = 35)
 entry_especialidade.pack()
 
+#entrada de unidade + autocomplete
+
+ttk.Label(appMain, text="Unidade:").pack()
+entry_unidade = ttk.Entry(appMain, width= 35)
+entry_unidade.pack(pady=1)
+
 
 # Botões
-#btn_tabuada = tk.Button(janela, text="Tabuada", command=abrir_tabuada)
-#btn_tabuada.pack(pady=5)
+
+planilha_unidade = tk.Button(appMain, text= "Listas de Login")
+btn_login = tk.Button(appMain, image=icone_tk ,text="Ver Logins Criados", compound="left")
+btn_login.pack(pady=5)
+botao_image = icone_tk
 
 
 appMain.mainloop()
