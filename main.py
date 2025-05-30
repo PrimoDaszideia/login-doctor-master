829e7fc654ea19576d683e9ec40f277dfc66fced
Lucas Silva <lucasprimoprofessional@gmail.com>
2025-05-30 16:38:15 -0300

    Partindo para finalizar o main

--- main.py
+++ main.py
@@ -14,13 +14,26 @@ appMain.iconbitmap("img/surgeon.ico")
 appMain.geometry("800x450")
 
 #Imagens do sistema
+ver_login = Image.open("img/eye.png")       # Substitua pelo caminho da sua imagem
+ver_login = ver_login.resize((24, 24))            # Redimensiona o ícone, se necessário
+ver_login_tk = ImageTk.PhotoImage(ver_login)
 
-icone = Image.open("img/surgeon.png")       # Substitua pelo caminho da sua imagem
-icone = icone.resize((24, 24))            # Redimensiona o ícone, se necessário
-icone_tk = ImageTk.PhotoImage(icone)
+excluir = Image.open("img/remover-usuario.png")
+excluir = excluir.resize((24,24))
+excluir_tk = ImageTk.PhotoImage(excluir)
+
+baixar_login = Image.open("img/download.png")
+baixar_login = baixar_login.resize((24, 24))
+baixar_login_tk = ImageTk.PhotoImage(baixar_login)
+
+copiar = Image.open("img/copiando.png")
+copiar = copiar.resize((24,24))
+copiar_tk = ImageTk.PhotoImage(copiar)
+
 excel = Image.open('img/extensao-de-formato-de-arquivo-xlsx.png')
 excel = excel.resize((24,24))
 excel_tk = ImageTk.PhotoImage(excel)
+
 adicionar_login = Image.open('img/adicionar-usuario.png')
 adicionar_login = adicionar_login.resize((24,24))
 adicionar_login_tk = ImageTk.PhotoImage(adicionar_login)
@@ -58,17 +71,30 @@ entry_unidade.pack(pady=1)
 
 # Botões
 
+#botão de adicionar
 bnt_adicionar = tk.Button(appMain, image= adicionar_login_tk ,text = " Adicionar Login", compound="left")
-bnt_adicionar.pack(pady=5)
-bnt_adicionar = adicionar_login_tk
+bnt_adicionar.pack(side= tk.LEFT, padx = (40,5))
+
+#botão de excluir logins
+bnt_excluir_logins = tk.Button(appMain, image= excluir_tk,  text= "Excluir Logins", compound="left")
+bnt_excluir_logins.pack(side= tk.LEFT, padx = 5)
+
+#botão de ver login
+btn_ver_login = tk.Button(appMain, image=ver_login_tk ,text="Ver Logins", compound="left")
+btn_ver_login.pack(side= tk.LEFT, padx= 5)
+
+#botão de copiar login
+bnt_copiar_login = tk.Button(appMain, image=copiar_tk ,text = 'Copiar Logins', compound="left" ,command= copiar_logins)
+bnt_copiar_login.pack(side= tk.LEFT, padx= 5)
+
+#botão de baixar login para xlsx
+bnt_baixar_login = tk.Button(appMain, image= baixar_login_tk, text = 'Exportar Logins', compound= "left", command= exportar_logins)
+bnt_baixar_login.pack(side= tk.LEFT, padx= 5)
+
+#botão de exportar unidades para xlsx
 bnt_unidade = tk.Button(appMain, image= excel_tk,text= " Exportar Unidades", compound="left",command=exportar_unidades)
-bnt_unidade.pack(pady=5)
-bnt_unidade = excel_tk
-btn_login = tk.Button(appMain, image=icone_tk ,text="Ver Logins", compound="left")
-btn_login.pack(pady=5)
-botao_image = icone_tk
-bnt_export_login = tk.Button(appMain, text = 'Exportar Login', command= exportar_logins)
-bnt_export_login.pack(pady=5)
+bnt_unidade.pack(side= tk.LEFT, padx= 5)
+
 
 
 appMain.mainloop()
